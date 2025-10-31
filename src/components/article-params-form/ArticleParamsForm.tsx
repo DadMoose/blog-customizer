4557654c9a68b8d3092e8439c4015ea6e201f875
DadMoose MacBook 15 <dadmoose@ya.ru>
2025-10-31 14:48:43 +0300

    feat: sidebar form: open/close, apply/reset, click-outside

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,13 +2,71 @@ import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { ArticleStateType } from 'src/constants/articleProps';
+import { FormEvent, useEffect, useRef } from 'react';
+import clsx from 'clsx';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	isOpen: boolean;
+	formState: ArticleStateType;
+	onChange: (state: Partial<ArticleStateType>) => void;
+	onApply: () => void;
+	onReset: () => void;
+	onClose: () => void;
+	onToggle: () => void;
+};
+
+export const ArticleParamsForm = ({
+	isOpen,
+	formState,
+	onChange,
+	onApply,
+	onReset,
+	onClose,
+	onToggle,
+}: ArticleParamsFormProps) => {
+	const containerRef = useRef<HTMLElement>(null);
+
+	useEffect(() => {
+		if (!isOpen) return;
+
+		const handleClickOutside = (event: MouseEvent) => {
+			if (
+				event.target instanceof HTMLElement &&
+				containerRef.current &&
+				!containerRef.current.contains(event.target)
+			) {
+				onClose();
+			}
+		};
+
+		window.addEventListener('mousedown', handleClickOutside);
+
+		return () => {
+			window.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, [isOpen, onClose]);
+
+	const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		onApply();
+	};
+
+	const handleReset = (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		onReset();
+	};
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton isOpen={isOpen} onClick={onToggle} />
+			<aside
+				className={clsx(styles.container, { [styles.container_open]: isOpen })}
+				ref={containerRef}>
+				<form
+					className={styles.form}
+					onSubmit={handleSubmit}
+					onReset={handleReset}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import {
+	ArticleStateType,
+	defaultArticleState,
+} from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -13,6 +16,30 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+	const [articleState, setArticleState] =
+		useState<ArticleStateType>(defaultArticleState);
+	const [formState, setFormState] =
+		useState<ArticleStateType>(defaultArticleState);
+
+	const handleToggleSidebar = () => {
+		setIsSidebarOpen((prevState) => !prevState);
+	};
+	const handleCloseSidebar = () => {
+		setIsSidebarOpen(false);
+	};
+	const handleApply = () => {
+		setArticleState(formState);
+		handleCloseSidebar();
+	};
+	const handleReset = () => {
+		setArticleState(defaultArticleState);
+		setFormState(defaultArticleState);
+	};
+	const handleFormChange = (updatedState: Partial<ArticleStateType>) => {
+		setFormState((prev) => ({ ...prev, ...updatedState }));
+	};
+
 	return (
 		<main
 			className={clsx(styles.main)}
@@ -25,7 +52,15 @@ const App = () => {
 					'--bg-color': defaultArticleState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm
+				isOpen={isSidebarOpen}
+				formState={formState}
+				onChange={handleFormChange}
+				onApply={handleApply}
+				onReset={handleReset}
+				onClose={handleCloseSidebar}
+				onToggle={handleToggleSidebar}
+			/>
 			<Article />
 		</main>
 	);
